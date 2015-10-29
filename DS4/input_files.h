5342ad8aed321fce49472d6de3e7b176c90b673e
Jan Kaspar <jan.kaspar@gmail.com>
2015-10-29 10:32:35 +0100

    Loop for multiple EVB processes.

--- DS4/input_files.h
+++ DS4/input_files.h
@@ -15,15 +15,21 @@ void InitInputFiles()
 
 	// 2015_10_16, fill 4505 (DS4)
 
-	std::string prefix = "root://eostotem.cern.ch//eos/totem/user/j/jkaspar/reco/sr,pass2/";
-	input_files.push_back(prefix + "9940_EVB11_1.0-9_ntuple.root");
-	input_files.push_back(prefix + "9940_EVB11_1.10-19_ntuple.root");
-	input_files.push_back(prefix + "9940_EVB11_1.20-29_ntuple.root");
-	input_files.push_back(prefix + "9940_EVB11_1.30-39_ntuple.root");
-	input_files.push_back(prefix + "9940_EVB11_1.40-49_ntuple.root");
-	input_files.push_back(prefix + "9940_EVB11_1.50-59_ntuple.root");
-	input_files.push_back(prefix + "9940_EVB11_1.60-69_ntuple.root");
-	input_files.push_back(prefix + "9950_EVB11_1.0-9_ntuple.root");
-	input_files.push_back(prefix + "9950_EVB11_1.10-19_ntuple.root");
-	input_files.push_back(prefix + "9952_EVB11_1.0-9_ntuple.root");
+	std::string prefix = "root://eostotem.cern.ch//eos/totem/data/cmstotem/2015/90m/Totem/Ntuple/version1/";
+
+	std::vector<std::string> EVBs = { "11_1" };
+
+	for (const auto &evb : EVBs)
+	{
+		input_files.push_back(prefix + "9940_EVB" + evb + ".0-9_ntuple.root");
+		input_files.push_back(prefix + "9940_EVB" + evb + ".10-19_ntuple.root");
+		input_files.push_back(prefix + "9940_EVB" + evb + ".20-29_ntuple.root");
+		input_files.push_back(prefix + "9940_EVB" + evb + ".30-39_ntuple.root");
+		input_files.push_back(prefix + "9940_EVB" + evb + ".40-49_ntuple.root");
+		input_files.push_back(prefix + "9940_EVB" + evb + ".50-59_ntuple.root");
+		input_files.push_back(prefix + "9940_EVB" + evb + ".60-69_ntuple.root");
+		input_files.push_back(prefix + "9950_EVB" + evb + ".0-9_ntuple.root");
+		input_files.push_back(prefix + "9950_EVB" + evb + ".10-19_ntuple.root");
+		input_files.push_back(prefix + "9952_EVB" + evb + ".0-9_ntuple.root");
+	}
 }
