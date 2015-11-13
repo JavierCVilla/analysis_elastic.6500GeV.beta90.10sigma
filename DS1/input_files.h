98ad439e7a2d93f54b4014758f493b8a1590cef8
Jan Kaspar <jan.kaspar@gmail.com>
2015-11-13 19:46:59 +0100

    Use official ntuples as input.

--- DS1/input_files.h
+++ DS1/input_files.h
@@ -15,6 +15,7 @@ void InitInputFiles()
 
 	// 2015_10_15, fill 4495 (DS1)
 
+	/*
 	//std::string prefix = "root://eostotem.cern.ch//eos/totem/data/cmstotem/2015/90m/Totem/Ntuple/version1/";
 	std::string prefix = "root://eostotem.cern.ch//eos/totem/user/j/jkaspar/reco/alignment_version2_sr_el/";
 
@@ -41,4 +42,67 @@ void InitInputFiles()
 		// TODO: uncomment
 		//input_files.push_back(prefix + "9883_EVB" + evb + ".36-40_ntuple.root");
 	}
+	*/
+
+	std::string prefix = "root://eostotem.cern.ch//eos/totem/data/cmstotem/2015/90m/Totem/Ntuple/version2/4495/";
+
+	input_files.push_back(prefix + "TotemNTuple_9874.ntuple.root");
+	input_files.push_back(prefix + "TotemNTuple_9877.000.ntuple.root");
+	input_files.push_back(prefix + "TotemNTuple_9877.001.ntuple.root");
+	input_files.push_back(prefix + "TotemNTuple_9877.002.ntuple.root");
+	input_files.push_back(prefix + "TotemNTuple_9877.003.ntuple.root");
+	input_files.push_back(prefix + "TotemNTuple_9877.004.ntuple.root");
+	input_files.push_back(prefix + "TotemNTuple_9877.005.ntuple.root");
+	input_files.push_back(prefix + "TotemNTuple_9877.006.ntuple.root");
+	input_files.push_back(prefix + "TotemNTuple_9877.007.ntuple.root");
+	input_files.push_back(prefix + "TotemNTuple_9877.008.ntuple.root");
+	input_files.push_back(prefix + "TotemNTuple_9877.009.ntuple.root");
+	input_files.push_back(prefix + "TotemNTuple_9877.010.ntuple.root");
+	input_files.push_back(prefix + "TotemNTuple_9879.000.ntuple.root");
+	input_files.push_back(prefix + "TotemNTuple_9879.001.ntuple.root");
+	input_files.push_back(prefix + "TotemNTuple_9879.002.ntuple.root");
+	input_files.push_back(prefix + "TotemNTuple_9881.000.ntuple.root");
+	input_files.push_back(prefix + "TotemNTuple_9881.001.ntuple.root");
+	input_files.push_back(prefix + "TotemNTuple_9881.002.ntuple.root");
+	input_files.push_back(prefix + "TotemNTuple_9881.003.ntuple.root");
+	input_files.push_back(prefix + "TotemNTuple_9881.004.ntuple.root");
+	input_files.push_back(prefix + "TotemNTuple_9881.005.ntuple.root");
+	input_files.push_back(prefix + "TotemNTuple_9881.006.ntuple.root");
+	input_files.push_back(prefix + "TotemNTuple_9881.007.ntuple.root");
+	input_files.push_back(prefix + "TotemNTuple_9883.000.ntuple.root");
+	input_files.push_back(prefix + "TotemNTuple_9883.001.ntuple.root");
+	input_files.push_back(prefix + "TotemNTuple_9883.002.ntuple.root");
+	input_files.push_back(prefix + "TotemNTuple_9883.003.ntuple.root");
+	input_files.push_back(prefix + "TotemNTuple_9883.004.ntuple.root");
+	input_files.push_back(prefix + "TotemNTuple_9883.005.ntuple.root");
+	input_files.push_back(prefix + "TotemNTuple_9883.006.ntuple.root");
+	input_files.push_back(prefix + "TotemNTuple_9883.007.ntuple.root");
+	input_files.push_back(prefix + "TotemNTuple_9883.008.ntuple.root");
+	input_files.push_back(prefix + "TotemNTuple_9883.009.ntuple.root");
+	input_files.push_back(prefix + "TotemNTuple_9883.011.ntuple.root");
+	input_files.push_back(prefix + "TotemNTuple_9883.012.ntuple.root");
+	input_files.push_back(prefix + "TotemNTuple_9883.013.ntuple.root");
+	input_files.push_back(prefix + "TotemNTuple_9883.014.ntuple.root");
+	input_files.push_back(prefix + "TotemNTuple_9883.015.ntuple.root");
+	input_files.push_back(prefix + "TotemNTuple_9883.016.ntuple.root");
+	input_files.push_back(prefix + "TotemNTuple_9883.017.ntuple.root");
+	input_files.push_back(prefix + "TotemNTuple_9883.018.ntuple.root");
+	input_files.push_back(prefix + "TotemNTuple_9883.019.ntuple.root");
+	input_files.push_back(prefix + "TotemNTuple_9883.021.ntuple.root");
+	input_files.push_back(prefix + "TotemNTuple_9883.022.ntuple.root");
+	input_files.push_back(prefix + "TotemNTuple_9883.023.ntuple.root");
+	input_files.push_back(prefix + "TotemNTuple_9883.024.ntuple.root");
+	input_files.push_back(prefix + "TotemNTuple_9883.025.ntuple.root");
+	input_files.push_back(prefix + "TotemNTuple_9883.026.ntuple.root");
+	input_files.push_back(prefix + "TotemNTuple_9883.027.ntuple.root");
+	input_files.push_back(prefix + "TotemNTuple_9883.028.ntuple.root");
+	input_files.push_back(prefix + "TotemNTuple_9883.029.ntuple.root");
+	input_files.push_back(prefix + "TotemNTuple_9883.032.ntuple.root");
+	input_files.push_back(prefix + "TotemNTuple_9883.033.ntuple.root");
+	input_files.push_back(prefix + "TotemNTuple_9883.034.ntuple.root");
+	input_files.push_back(prefix + "TotemNTuple_9883.035.ntuple.root");
+	input_files.push_back(prefix + "TotemNTuple_9883.036.ntuple.root");
+	input_files.push_back(prefix + "TotemNTuple_9883.038.ntuple.root");
+	input_files.push_back(prefix + "TotemNTuple_9883.039.ntuple.root");
+	input_files.push_back(prefix + "TotemNTuple_9883.040.ntuple.root");
 }
--- DS2/input_files.h
+++ DS2/input_files.h
@@ -15,6 +15,7 @@ void InitInputFiles()
 
 	// 2015_10_15, fill 4496 (DS2)
 
+	/*
 	//std::string prefix = "root://eostotem.cern.ch//eos/totem/data/cmstotem/2015/90m/Totem/Ntuple/version1/";
 	std::string prefix = "root://eostotem.cern.ch//eos/totem/user/j/jkaspar/reco/alignment_version2_sr_el/";
 
@@ -34,4 +35,60 @@ void InitInputFiles()
 		input_files.push_back(prefix + "9898_EVB" + evb + ".10-19_ntuple.root");
 		input_files.push_back(prefix + "9898_EVB" + evb + ".20-29_ntuple.root");
 	}
+	*/
+
+	std::string prefix = "root://eostotem.cern.ch//eos/totem/data/cmstotem/2015/90m/Totem/Ntuple/version2/4496/";
+
+	input_files.push_back(prefix + "TotemNTuple_9893.000.ntuple.root");
+	input_files.push_back(prefix + "TotemNTuple_9893.001.ntuple.root");
+	input_files.push_back(prefix + "TotemNTuple_9893.002.ntuple.root");
+	input_files.push_back(prefix + "TotemNTuple_9893.003.ntuple.root");
+	input_files.push_back(prefix + "TotemNTuple_9893.004.ntuple.root");
+	input_files.push_back(prefix + "TotemNTuple_9893.005.ntuple.root");
+	input_files.push_back(prefix + "TotemNTuple_9893.006.ntuple.root");
+	input_files.push_back(prefix + "TotemNTuple_9893.007.ntuple.root");
+	input_files.push_back(prefix + "TotemNTuple_9894.000.ntuple.root");
+	input_files.push_back(prefix + "TotemNTuple_9894.001.ntuple.root");
+	input_files.push_back(prefix + "TotemNTuple_9894.002.ntuple.root");
+	input_files.push_back(prefix + "TotemNTuple_9894.003.ntuple.root");
+	input_files.push_back(prefix + "TotemNTuple_9894.004.ntuple.root");
+	input_files.push_back(prefix + "TotemNTuple_9894.005.ntuple.root");
+	input_files.push_back(prefix + "TotemNTuple_9894.006.ntuple.root");
+	input_files.push_back(prefix + "TotemNTuple_9894.007.ntuple.root");
+	input_files.push_back(prefix + "TotemNTuple_9894.008.ntuple.root");
+	input_files.push_back(prefix + "TotemNTuple_9894.009.ntuple.root");
+	input_files.push_back(prefix + "TotemNTuple_9894.010.ntuple.root");
+	input_files.push_back(prefix + "TotemNTuple_9894.011.ntuple.root");
+	input_files.push_back(prefix + "TotemNTuple_9894.012.ntuple.root");
+	input_files.push_back(prefix + "TotemNTuple_9894.013.ntuple.root");
+	input_files.push_back(prefix + "TotemNTuple_9895.000.ntuple.root");
+	input_files.push_back(prefix + "TotemNTuple_9895.001.ntuple.root");
+	input_files.push_back(prefix + "TotemNTuple_9895.002.ntuple.root");
+	input_files.push_back(prefix + "TotemNTuple_9895.003.ntuple.root");
+	input_files.push_back(prefix + "TotemNTuple_9895.004.ntuple.root");
+	input_files.push_back(prefix + "TotemNTuple_9895.005.ntuple.root");
+	input_files.push_back(prefix + "TotemNTuple_9896.000.ntuple.root");
+	input_files.push_back(prefix + "TotemNTuple_9896.001.ntuple.root");
+	input_files.push_back(prefix + "TotemNTuple_9896.002.ntuple.root");
+	input_files.push_back(prefix + "TotemNTuple_9896.003.ntuple.root");
+	input_files.push_back(prefix + "TotemNTuple_9896.004.ntuple.root");
+	input_files.push_back(prefix + "TotemNTuple_9896.005.ntuple.root");
+	input_files.push_back(prefix + "TotemNTuple_9896.006.ntuple.root");
+	input_files.push_back(prefix + "TotemNTuple_9896.007.ntuple.root");
+	input_files.push_back(prefix + "TotemNTuple_9896.008.ntuple.root");
+	input_files.push_back(prefix + "TotemNTuple_9896.009.ntuple.root");
+	input_files.push_back(prefix + "TotemNTuple_9896.010.ntuple.root");
+	input_files.push_back(prefix + "TotemNTuple_9896.011.ntuple.root");
+	input_files.push_back(prefix + "TotemNTuple_9896.012.ntuple.root");
+	input_files.push_back(prefix + "TotemNTuple_9896.013.ntuple.root");
+	input_files.push_back(prefix + "TotemNTuple_9896.014.ntuple.root");
+	input_files.push_back(prefix + "TotemNTuple_9896.015.ntuple.root");
+	input_files.push_back(prefix + "TotemNTuple_9896.016.ntuple.root");
+	input_files.push_back(prefix + "TotemNTuple_9896.017.ntuple.root");
+	input_files.push_back(prefix + "TotemNTuple_9896.018.ntuple.root");
+	input_files.push_back(prefix + "TotemNTuple_9896.019.ntuple.root");
+	input_files.push_back(prefix + "TotemNTuple_9896.020.ntuple.root");
+	input_files.push_back(prefix + "TotemNTuple_9896.021.ntuple.root");
+	input_files.push_back(prefix + "TotemNTuple_9896.022.ntuple.root");
+	input_files.push_back(prefix + "TotemNTuple_9896.023.ntuple.root");
 }
